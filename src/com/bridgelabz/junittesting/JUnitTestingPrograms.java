053e110894887be0e22b2330ae6a0ce7672a8ee6
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-25 13:50:40 +0530

    Added:code calculate minimum number of notes

--- src/com/bridgelabz/junittesting/JUnitTestingPrograms.java
+++ src/com/bridgelabz/junittesting/JUnitTestingPrograms.java
@@ -1,7 +1,30 @@
 package com.bridgelabz.junittesting;
 
 public class JUnitTestingPrograms {
-    public static void main(String[] args) {
-        System.out.println("programs junit testing");
+
+        public static void countNotes(int amount) {
+            int notes[] = {1000, 500, 100, 50, 10, 5, 2, 1};
+            int noteFreq[] = new int[notes.length];
+            int totalNotes=0;
+            for (int i = 0; i < notes.length; i++) {
+                if (amount >= notes[i]) {
+                    noteFreq[i] = amount / notes[i];
+                    amount = amount - noteFreq[i] * notes[i];
+                }
+            }
+            System.out.println();
+            for (int i = 0; i < notes.length; i++) {
+                if (noteFreq[i] != 0) {
+                    System.out.println(notes[i] + "  :  " + noteFreq[i]);
+                    totalNotes+=noteFreq[i];
+                }
+            }
+            System.out.println("minimum number of notes is : "+totalNotes);
+        }
+        public static void main(String[] args) {
+            int amount = 1072;
+            countNotes(amount);
+
+
     }
 }
